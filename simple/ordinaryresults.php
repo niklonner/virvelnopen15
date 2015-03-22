718345843d081b4c24833febe4fd1a92329a33ca
Niklas Lönnerfors <niklas_lonnerfors@yahoo.se>
2015-03-22 20:37:16 +0100

    simple result page now displays basic view of gateways

--- simple/ordinaryresults.php
+++ simple/ordinaryresults.php
@@ -7,6 +7,10 @@ require_once '../db/dbfuncs.php';
 <body>
     <a href="index.php">&lt;&lt; Tillbaka</a>
   <h1>Totala resultat</h1>
+  <p style="font-weight:bold">
+    (D) = dam<br/>
+    (J) = junior
+  </p>
   <table>
     <tr>
       <th>Pos.</th>
@@ -21,17 +25,29 @@ require_once '../db/dbfuncs.php';
       <th>Scratch</th>
       <th>Hcp/serie</th>
       <th>Resultat</th>
+      <th>Final?</th>
     </tr>
 <?php
 $squads_by_id = getNumberOfPlayedSquadsPerPlayer();
 $i = 1;
-foreach (getOrdinaryResults() as $result) {
+$earlybirdcounter = 1;
+foreach (getCompleteResults() as $result) {
   $squadnum = $squads_by_id[$result[id]];
-  $reentry = $squadnum == 1 ? "" : ($squadnum == 2 ? "(R)" :"(R2)");
+  $reentry = $squadnum == 1 ? "" : ($squadnum == 2 ? " (R)" :" (R2)");
+  $femalestring = $result[isfemale] ? " (D)" : "";
+  $juniorstring = $result[isjunior] ? " (J)" : "";
+  $finalsstring = $result[infinals] ?
+    ($result[way]=="ordinary" ? "Ja" : 
+      ($result[way]=="junior" ? "Ja, bästa junior" : 
+        ($result[way]=="female" ? "Ja, bästa dam" : 
+          ($result[way]=="earlybird" ? "Ja, early bird ". $earlybirdcounter++ : "")
+        )
+      )
+    ) : "";
   echo <<<EOT
 <tr>
   <td>$i.</td>
-  <td>$result[lastname] $reentry</td>
+  <td>$result[lastname]$femalestring$juniorstring$reentry</td>
   <td>$result[club]</td>
   <td>$result[s1]</td>
   <td>$result[s2]</td>
@@ -42,6 +58,7 @@ foreach (getOrdinaryResults() as $result) {
   <td>$result[scratch]</td>
   <td>$result[hcp]</td>
   <td>$result[result]</td>
+  <td style="font-weight:bold">$finalsstring</td>
 </tr>
 EOT;
   $i++;
